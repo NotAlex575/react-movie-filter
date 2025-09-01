6f9780b362422479ad257ccb622e15fdc5a9cb37
NotAlex575 <dapontealessandro89gmail.com>
2025-09-01 18:11:05 +0200

    implementata parte style header

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return(
         <header>
-            <h1>React Fil Filter Esercizio</h1>
+            <h1>React Film Filter Esercizio</h1>
         </header>
     )
 };
