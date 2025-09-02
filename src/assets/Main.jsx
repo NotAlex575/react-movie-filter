0bd2aa82d8a119946c3cd2c0183994a30c2a8e05
NotAlex575 <dapontealessandro89gmail.com>
2025-09-02 10:51:47 +0200

    eliminati commenti ripetitivi

--- src/assets/Main.jsx
+++ src/assets/Main.jsx
@@ -14,9 +14,9 @@ const FilmList =  [
 const Main = () => {
     //lista film iniziale
     const [films, setFilms] = useState(FilmList)
-    //lista film filtrati 
+    //lista film filtrati per genere
     const [searchGenre, setSearchGenre] = useState("");
-    //lista film filtrati 
+    //lista film filtrati per titolo
     const [searchTitle, setSearchTitle] = useState("");
 
     //non appena viene eseguito un filter (tramite input), 
@@ -38,24 +38,20 @@ const Main = () => {
     }, [searchGenre]); //questo useEffect si esegue ogni volta 
                   //che "seach" (in input->value) cambia
 
-    //non appena viene eseguito un filter (tramite input), 
-    //la lista aggiorna il contenuto in base a quello che abbiamo
-    //cercato
+
     useEffect(() => {
 
-        //uso il filter per aggiornare la lista in base alla value
-        //che viene inserita nell'input(se si trova la mette nella lista)
+        
         const filteredFilms = FilmList.filter((film) => {
 
-            //converte la ricerca del genere del film in minuscolo
+            //converte la ricerca del titolo del film in minuscolo
             return film.title.toLowerCase().includes(searchTitle.toLowerCase())
         })
 
         //aggiorna la lista con i film filtrati
         setFilms(filteredFilms);
         
-    }, [searchTitle]); //questo useEffect si esegue ogni volta 
-                  //che "seach" (in input->value) cambia
+    }, [searchTitle]); 
 
 
     return (
@@ -90,8 +86,6 @@ const Main = () => {
                     className="w-100per"
                     value={searchTitle}
                     onChange={(event) => setSearchTitle(event.target.value)}
-                    //ogni volta che si inserisce un contenuto qui (che si trovi o non si trovi nella lista),
-                    //la lista si aggiorna
                 />
             </div>
         </main>
